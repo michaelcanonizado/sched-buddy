16e050109b6de2fc432357b5ed8e26e794e9db19
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2026-05-16 15:03:34 +0800

    feat: add color picker to add schdule background fill

--- web/src/features/schedule/components/actions/change-background/add-fill.tsx
+++ web/src/features/schedule/components/actions/change-background/add-fill.tsx
@@ -1,16 +1,33 @@
 import { TimetableSnapshot } from '@/features/canvas-engine/canvas-engine'
 import { useCanvasEngine } from '@/features/canvas-engine/use-canvas-engine-store'
 import { cn } from '@/lib/utils'
-import { ComponentClassNameProp } from '@/types'
+import { ComponentChildrenProp, ComponentClassNameProp } from '@/types'
 import Image from 'next/image'
 import { useEffect, useMemo, useRef, useState } from 'react'
+
+import {
+  ColorArea,
+  ColorField,
+  ColorPicker as Picker,
+  ColorSlider,
+  ColorSwatch,
+  ColorSwatchPicker,
+  ColorSwatchPickerItem,
+  ColorThumb,
+  SliderTrack,
+} from '@/components/ui/color'
+import { parseColor, Input as ColorInput, Color } from 'react-aria-components'
+import { inputClassNames } from '@/components/ui/input'
+import { TextBody } from '@/components/text'
+import { Button } from '@/components/ui/button'
+import { DialogFooter } from '@/components/ui/dialog'
 
 interface MimicCanvasProps {
   snapshot: TimetableSnapshot
   backgroundColor: string
 }
 
-export function MimicCanvas({
+function MimicCanvas({
   snapshot,
   backgroundColor,
   className,
@@ -77,9 +94,37 @@ export function MimicCanvas({
   )
 }
 
+function ColorPicker({ onChange, value }: { value: Color; onChange: (value: Color) => void }) {
+  return (
+    <Picker value={value} onChange={onChange}>
+      <div className='flex w-full flex-col gap-0 outline-none'>
+        <ColorArea
+          colorSpace='hsb'
+          xChannel='saturation'
+          yChannel='brightness'
+          className='h-[100px] w-full rounded-b-none border-b-0'
+        >
+          <ColorThumb className='z-50' />
+        </ColorArea>
+        <ColorSlider colorSpace='hsb' channel='hue'>
+          <SliderTrack className='w-full rounded-t-none border-t-0'>
+            <ColorThumb className='top-1/2' />
+          </SliderTrack>
+        </ColorSlider>
+      </div>
+      <div className='flex w-full flex-col gap-1 *:grow'>
+        <TextBody className='text-center'>Hex</TextBody>
+        <ColorField className='flex flex-col gap-2' aria-label='hex color'>
+          <ColorInput className={cn(inputClassNames, 'text-center')} />
+        </ColorField>
+      </div>
+    </Picker>
+  )
+}
+
 export default function AddFill({ setDialogOpen }: { setDialogOpen: (open: boolean) => void }) {
   const canvasEngine = useCanvasEngine()
-  const [backgroundColor, setBackgroundColor] = useState('#fff')
+  const [backgroundColor, setBackgroundColor] = useState<Color>(parseColor('#ffffff'))
 
   const snapshot = useMemo(() => {
     if (!canvasEngine) return null
@@ -90,16 +135,24 @@ export default function AddFill({ setDialogOpen }: { setDialogOpen: (open: boole
   }, [canvasEngine])
 
   return (
-    <div className='flex h-[500px] w-[750px] flex-col gap-8 p-8'>
-      <div className=''>
-        {snapshot && (
-          <MimicCanvas
-            className='rounded-lg'
-            snapshot={snapshot}
-            backgroundColor={backgroundColor}
-          />
-        )}
+    <>
+      <div className='bg-muted h-fit max-h-[650px] w-[600px] overflow-y-scroll'>
+        <div className='flex flex-col gap-4 p-8'>
+          <div className=''>
+            {snapshot && (
+              <MimicCanvas
+                className='rounded-lg'
+                snapshot={snapshot}
+                backgroundColor={`#${backgroundColor.toHexInt().toString(16).padStart(6, '0')}`}
+              />
+            )}
+          </div>
+          <ColorPicker value={backgroundColor} onChange={setBackgroundColor} />
+        </div>
       </div>
-    </div>
+      <DialogFooter>
+        <Button>Confirm</Button>
+      </DialogFooter>
+    </>
   )
 }
